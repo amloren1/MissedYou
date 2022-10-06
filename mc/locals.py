db2e4dbccbd89506cd48d9f6e89b6803da5cc872
amloren1 <alexlorenzo39@gmail.com>
2022-10-06 04:30:35 +0000

    move scrapper code into obj

--- mc/locals.py
+++ mc/locals.py
@@ -1,24 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.craigslist.org/about/sites"
+from const import SITES_URL
 
 
-def get_city_links(state="Arizona"):
+def get_city_links(locals: list =["Arizona"]):
     
-    r = requests.get(URL)
+    r = requests.get(SITES_URL)
 
     # parse html
     soup = BeautifulSoup(r.text, "html.parser")
 
     # find all links
     links = soup.find_all("a", {"class": "result-title hdrlnk"})
-
     # find links for each state heading
     states = soup.find_all("h4")
+    city_links = []
     for state in states:
-        print(state.text)
-        # get all links in state
-        state_links = state.find_next_sibling().find_all("a")
-        for state_link in state_links:
-            print(state_link.get("href"))
+        if state.text in locals:
+            # print(state.text)
+            # get all links in state
+            state_links = state.find_next_sibling().find_all("a")
+
+            for state_link in state_links:
+                print(state_link.get("href"))
+                print(state_link.text)
+                city_links.append(state_link.get("href"))
+    return city_links
+
+
+get_city_links()
--- mc/mc.py
+++ mc/mc.py
@@ -6,102 +6,118 @@ Simple webscraper using beautiful soup
 # Import modules
 import requests
 from bs4 import BeautifulSoup
-import re
-import os
-import sys
 
 import time
 
-# read in craigs_list.txt
-with open("craigs_list.txt", "r") as f:
-    # read in the lines
-    locations = f.readlines()
-    f.close()
+from const import US_STATES
+from locals import get_city_links
 
-# craigslist phoenix missed connections base url
-base_url = "https://{}.craigslist.org/search/mis"
-
-
-def get_page_main_text_body(url):
-    """
-    Get the main text body of the page
-    """
-    # Get the page
-    page = requests.get(url)
-    # Parse the page
-    soup = BeautifulSoup(page.content, "html.parser")
-    # Get the main text body
-    main_text_body = soup.find(id="postingbody")
-
-    # get only the text
-    main_text_body = main_text_body.text
-    # Return the main text body
-    return main_text_body.split("\n")[-2]
+# # read in craigs_list.txt
+# with open("craigs_list.txt", "r") as f:
+#     # read in the lines
+#     locations = f.readlines()
+#     f.close()
 
 
-for locale in locations:
-    base_url = base_url.format(locale.strip())
+class Scrapper:
 
-    while True:
-        # request base url for search result
-        r = requests.get(base_url)
+    def __init__(self, locations = US_STATES) -> None:
+        pass
 
-        # parse html
-        soup = BeautifulSoup(r.text, "html.parser")
+        # craigslist missed connections base url. format
+        self.url_path = "{}/search/mis"
+        self.locations = locations
+        self.city_links = get_city_links(locations)
 
-        # find all links
-        links = soup.find_all("a", {"class": "result-title hdrlnk"})
 
-        # extract next page link
-        next_page = soup.find("a", {"title": "next page"})
-        if next_page:
-            next_page_link = next_page.get("href", None)
+    @staticmethod
+    def _get_page_main_text_body(url):
+        """
+        Get the main text body of the page
+        """
+        # Get the page
+        page = requests.get(url)
+        # Parse the page
+        soup = BeautifulSoup(page.content, "html.parser")
+        # Get the main text body
+        main_text_body = soup.find(id="postingbody")
 
-        texts = []
-        # find all links in search result
-        for link in links:
-            # get link text
-            link_text = link.text
-            # get link url
-            link_url = link.get("href")
-            # print link text and url
-            print(link_text)
-            print(link_url)
-            # breakpoint()
-            # store body text to file next line
+        # get only the text
+        main_text_body = main_text_body.text
+        # Return the main text body
+        return main_text_body.split("\n")[-2]
 
-            # use asyncio to append the
-            # body text to the file
-            # store text asynchronously
+    def get_location_urls(self):
+        get_city_links(self.locations)
+        pass
 
-            # with open('res.txt', 'a') as f:
-            #     f.write(get_page_main_text_body(link_url))
-            #     f.write('\n')
-            #     f.close()
 
-            print(get_page_main_text_body(link_url))
-            texts.append(get_page_main_text_body(link_url))
-            # request link url
-            # r = requests.get(link_url)
-            # parse html and return the main text
+    def get_posts(self):
+        for locale in self.city_links:
+            breakpoint()
+            # add base to url path
+            url = self.url_path.format(locale)
+            print(url)
+            while True:
+                # request base url for search result
+                r = requests.get(url)
 
-            # # for p_tag in p_tags:
-            # #     # get p tag text
-            # #     p_tag_text = p_tag.text
-            # #     # print p tag text
-            # #     print(p_tag_text)
+                # parse html
+                soup = BeautifulSoup(r.text, "html.parser")
 
-            print("")
-            # time.sleep(5)
+                # find all links
+                links = soup.find_all("a", {"class": "result-title hdrlnk"})
 
-        # if next page link exists
-        if next_page_link:
-            # set base url to next page link
-            base_url = next_page_link
-        else:
-            # exit
-            with open("res.txt", "a") as f:
-                f.write("\n\n".join(texts))
-                f.close()
-            texts = None
-            break
+                # extract next page link
+                next_page = soup.find("a", {"title": "next page"})
+                if next_page:
+                    next_page_link = next_page.get("href", None)
+
+                texts = []
+                # find all links in search result
+                for link in links:
+                    # get link text
+                    link_text = link.text
+                    # get link url
+                    link_url = link.get("href")
+                    # print link text and url
+                    print(link_text)
+                    print(link_url)
+                    # breakpoint()
+                    # store body text to file next line
+
+                    # use asyncio to append the
+                    # body text to the file
+                    # store text asynchronously
+
+                    # with open('res.txt', 'a') as f:
+                    #     f.write(get_page_main_text_body(link_url))
+                    #     f.write('\n')
+                    #     f.close()
+
+                    print(get_page_main_text_body(link_url))
+                    texts.append(get_page_main_text_body(link_url))
+                    # request link url
+                    # r = requests.get(link_url)
+                    # parse html and return the main text
+
+                    # # for p_tag in p_tags:
+                    # #     # get p tag text
+                    # #     p_tag_text = p_tag.text
+                    # #     # print p tag text
+                    # #     print(p_tag_text)
+
+                    print("")
+                    # time.sleep(5)
+
+                # if next page link exists
+                if next_page_link:
+                    # set base url to next page link
+                    base_url = next_page_link
+                else:
+                    # exit
+                    with open("res.txt", "a") as f:
+                        f.write("\n\n".join(texts))
+                        f.close()
+                    texts = None
+                    break
